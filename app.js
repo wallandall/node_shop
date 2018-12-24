004c3317807e5c745469c7a9ebdfbe2d357b52d9
wallandall <wallandall@gmail.com>
2018-12-24 19:24:27 +0100

    inital Project Setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Add static path PUBLIC
+app.use(express.static(path.join(__dirname, 'public')));
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3002);
