4b824bf97c8f2d984d6497b3622701d4e9012dda
wallandall <wallandall@gmail.com>
2018-12-25 09:55:19 +0100

    added views with EJS

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-//Add static path PUBLIC
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3002);
