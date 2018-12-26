bea527dc9753b850a53eec53c4f9a3aa03e6f631
wallandall <wallandall@gmail.com>
2018-12-26 16:23:14 +0100

    Updated models and controller for adding/deleting/updating products and cart

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3002);
+app.listen(3000);
